9b7782424d11b8cef399b995011abd56a062e3e8
dofalami <polina.chugaeva@gmail.com>
2022-10-18 02:32:07 +0300

    add

--- src/brain-even-game.js
+++ src/brain-even-game.js
@@ -16,6 +16,6 @@ export default function brainEvenGame() {
   }
 
   if (randomNumber % 2 === 1 && answer === 'yes') {
-    console.log(`'${answer}' is wrong answer ;(. Correct answer was 'no'. Let's try again, ${userName}!`);
+    console.log(`'${answer}' is wrong answer ;(. Correct answer was 'no'. Let's try again, ${userName(userName)}!`);
   }
 };

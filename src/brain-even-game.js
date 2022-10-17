3b698e87ba880b413685a43173e2d7a895a1f474
dofalami <polina.chugaeva@gmail.com>
2022-10-18 00:53:55 +0300

    add

--- src/brain-even-game.js
+++ src/brain-even-game.js
@@ -2,7 +2,7 @@ import userName from '../src/cli.js';
 import readlineSync from 'readline-sync';
 import getRandom from '../src/randomNum.js';
 
-export default brainEvenGame = () => {
+export default function brainEvenGame() {
   userName();
 
 console.log('Answer "yes" if the number is even, otherwise answer "no". \n Question: ' + getRandom());

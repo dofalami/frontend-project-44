b21b39aeca0af3a012c0917a6ab8f21209eb8c0d
dofalami <polina.chugaeva@gmail.com>
2022-10-18 00:55:20 +0300

    aa

--- src/brain-even-game.js
+++ src/brain-even-game.js
@@ -5,7 +5,7 @@ import getRandom from '../src/randomNum.js';
 export default function brainEvenGame() {
   userName();
 
-console.log('Answer "yes" if the number is even, otherwise answer "no". \n Question: ' + getRandom());
+console.log('Answer "yes" if the number is even, otherwise answer "no".\nQuestion: ' + getRandom());
 
 const question = readlineSync.question('Your answer: ');
 console.log(question);

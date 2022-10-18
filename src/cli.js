c982f8ed0a8a59e26d6655e4b8f954f8f94facf1
dofalami <polina.chugaeva@gmail.com>
2022-10-18 03:11:56 +0300

    add

--- src/cli.js
+++ src/cli.js
@@ -1,6 +1,6 @@
 import readlineSync from 'readline-sync';
 
-export default function greeting() {
+export function greeting() {
     console.log('Welcome to the Brain Games!');
 
     const userName = readlineSync.question('May I have your name? ');

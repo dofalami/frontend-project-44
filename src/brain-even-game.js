51c7343e555f2393d5587d547ff4185d86b88bd6
dofalami <polina.chugaeva@gmail.com>
2022-10-18 23:08:37 +0300

    update

--- src/brain-even-game.js
+++ src/brain-even-game.js
@@ -34,6 +34,7 @@ export default function brainEvenGame() {
         break;
       }
     }
+    console.log(`Congratulations, ${userName}!`);
   }
-  console.log(`Congratulations, ${userName}!`);
+//  console.log(`Congratulations, ${userName}!`);
 };

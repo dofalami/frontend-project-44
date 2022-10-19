d0e5bd207c23f35595131e581697399219938ad7
dofalami <polina.chugaeva@gmail.com>
2022-10-19 04:37:56 +0300

    upd

--- src/games/brain-calc-game.js
+++ src/games/brain-calc-game.js
@@ -9,22 +9,23 @@ export default function brainCalcGame() {
   console.log(`Hello, ${userName}!`);
   console.log('What is the result of the expression?');
 
-    let i = 0;
+  let i = 0;
   for (; i < 3;) {
     const randomNumber = getRandom() + randomOperator() + getRandom();
+    const correctAnswer = randomNumber;
     console.log(`Question: ${randomNumber}`);
 
     const answer = readlineSync.question('Your answer: ');
 
-    if (randomNumber === answer) {
+    if (correctAnswer === answer) {
         console.log('Correct!');
         i += 1;
       } else {
-        console.log(`'${answer}' is wrong answer ;(. Correct answer was 'yes'. Let's try again, ${userName}!`);
+        console.log(`'${answer}' is wrong answer ;(. Correct answer was 'correctAnswer'. Let's try again, ${userName}!`);
         break;
       }
     }
-  }
-  if (i >= 3) {
-    console.log(`Congratulations, ${userName}!`);
+//  if (i >= 3) {
+//    console.log(`Congratulations, ${userName}!`);
+}
 }

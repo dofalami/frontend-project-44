4e2cf689e574dd32bf7fbc2b540e39d12744ddb9
dofalami <polina.chugaeva@gmail.com>
2022-10-19 04:59:42 +0300

    upd

--- src/games/brain-calc-game.js
+++ src/games/brain-calc-game.js
@@ -13,7 +13,7 @@ export default function brainCalcGame() {
   let i = 0;
   for (; i < 3;) {
 //    const randomNumber = `${getRandom}` `${randomOperator}` `${getRandom}`;
-    const randomNumber = getRandom + randomOperator + getRandom;
+    const randomNumber = getRandom(getRandom) + randomOperator(randomOperator) + getRandom(getRandom);
     const correctAnswer = randomNumber;
     console.log(`Question: ${randomNumber}`);
 

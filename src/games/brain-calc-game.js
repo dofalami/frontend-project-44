6995ad520058377743d47b25709c707ca6566398
dofalami <polina.chugaeva@gmail.com>
2022-10-20 05:47:43 +0300

    upd

--- src/games/brain-calc-game.js
+++ src/games/brain-calc-game.js
@@ -1,35 +1,18 @@
 // import readlineSync from 'readline-sync';
-import * as gameLogic from '../index.js';
+import run from '../index.js';
 import getRandom from '../randomNum.js';
 import randomOperator from '../randomOperator.js';
 
-export default function brainCalcGame() {
-  gameLogic();
-
   const firstQuestion = 'What is the result of the expression?';
-
+  const questionAnswer = () => {
   const number1 = `${getRandom()}`;
   const number2 = `${getRandom()}`;
   const operator = `${randomOperator()}`;
 
   const expression = number1 + ' ' + operator + ' ' + number2;
   const correctAnswer = eval(expression);
+  }
 
-  questionAnswer();
-
- // gameLogic();
-
-//  console.log('Question: ' + number1 + ' ' + operator + ' ' + number2);
-//  console.log(userAnswer);
-
-//  if (correctAnswer == yourAnswer) {
-//    console.log('Correct!');
-//    i += 1;
-//  } else {
-//      return console.log(`'${yourAnswer}' is wrong answer ;(. Correct answer was '${correctAnswer}'. Let's try again, ${userName}!`);
-//    }
-}
-
-// export { expression };
-
-
+  export default () => {
+    run (firstQuestion, questionAnswer);
+  };
--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 // логика игр
 import readlineSync from 'readline-sync';
 
-export default gameLogic = (firstQuestion, questionAnswer) => {
+export default (firstQuestion, questionAnswer) => {
   console.log('Welcome to the Brain Games!');
 
   const name = () => readlineSync.question('May I have your name? ');

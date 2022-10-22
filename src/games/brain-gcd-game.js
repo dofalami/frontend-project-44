a386f4186559aba3dbcf1c02be88a4ceb46a2e47
dofalami <polina.chugaeva@gmail.com>
2022-10-22 07:33:22 +0300

    add brain-gcd

---
+++ src/games/brain-gcd-game.js
@@ -0,0 +1,10 @@
+import run from '../index.js';
+import getRandom from '../randomNum.js';
+
+const firstQuestion = 'Find the greatest common divisor of given numbers.';
+
+const questionAnswer = () => {
+    const randomNumber = getRandom();
+    const expression = randomNumber + ' ' + randomNumber;
+    let correctAnswer = '';
+}

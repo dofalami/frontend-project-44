3fc0335947d3de608094154c08d46908018d08fe
dofalami <polina.chugaeva@gmail.com>
2022-10-27 05:48:19 +0300

    upd

--- src/games/brain-gcd-game.js
+++ src/games/brain-gcd-game.js
@@ -4,13 +4,13 @@ import getRandom from '../randomNum.js';
 const firstQuestion = 'Find the greatest common divisor of given numbers.';
 
 const questionAnswer = () => {
-    const randomNumber1 = getRandom();
-    const randomNumber2 = getRandom();
+    const randomNumber1 = `${getRandom()}`;
+    const randomNumber2 = `${getRandom()}`;
     const expression = randomNumber1 + ' ' + randomNumber2;
     let correctAnswer = '';
 
     let divisor = '';
-    if (randomNumber1 >= randomNumber2) {
+    if (randomNumber1 <= randomNumber2) {
         divisor = randomNumber1;
     }
     else {
@@ -19,8 +19,8 @@ const questionAnswer = () => {
     
     while (expression % divisor !== 0) {
         divisor = divisor - 1;
-        correctAnswer = divisor;
     }
+    correctAnswer = divisor;
 
     return [expression, correctAnswer];
 }

59772c200040ef323bf086dd3b4c3695e16293fc
dofalami <polina.chugaeva@gmail.com>
2022-10-31 02:31:25 +0300

    upd

--- src/games/brain-gcd-game.js
+++ src/games/brain-gcd-game.js
@@ -8,7 +8,7 @@ const maxNum = 100;
 const questionAnswer = () => {
   const randomNumber1 = getRandom(minNum, maxNum);
   const randomNumber2 = getRandom(minNum, maxNum);
-  const expression = String(`${andomNumber1}`) + ' ' + String(`${andomNumber1}`);
+  const expression = String(`${randomNumber1}`) + ' ' + String(`${randomNumber1}`);
 
   let correctAnswer = '';
 

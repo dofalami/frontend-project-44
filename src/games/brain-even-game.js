4ad686a9fa6a7400e330512b0e6d47c7eeca7654
dofalami <polina.chugaeva@gmail.com>
2022-10-30 22:13:10 +0300

    upd

--- src/games/brain-even-game.js
+++ src/games/brain-even-game.js
@@ -2,10 +2,13 @@ import run from '../index.js';
 import { getRandom } from '../randomNum.js';
 
 const firstQuestion = 'Answer "yes" if the number is even, otherwise answer "no".';
+const minNum = 1;
+const maxNum = 100;
 
 const questionAnswer = () => {
-  const randomNumber = getRandom();
-  const expression = eval(randomNumber);
+  const randomNumber = getRandom(minNum, maxNum);
+  let expressionNum = String(randomNumber);
+  const expression = expressionNum;
   let correctAnswer = '';
   
   if (expression % 2 === 0) {

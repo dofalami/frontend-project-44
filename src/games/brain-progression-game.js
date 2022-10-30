030b5aa48f3694d71f7c1ae91eb8499068aae10b
dofalami <polina.chugaeva@gmail.com>
2022-10-30 20:40:49 +0300

    upd

--- src/games/brain-progression-game.js
+++ src/games/brain-progression-game.js
@@ -32,7 +32,8 @@ const questionAnswer = () => {
     // ПЕРЕДЕЛАТЬ РАНДОМ ИНДЕКС (ОТ 0 ДО 9)
     let startIndex = getRandomMin();;
     let numberOfRemove = 1;
-    const expression = progression.splice(startIndex, numberOfRemove, '..');
+    let questionForUser = progression.splice(startIndex, numberOfRemove, '..');
+    const expression = questionForUser;
     const correctAnswer = 42;
 
     return [expression, correctAnswer];

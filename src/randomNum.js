d03efb5c35561ab5fe676795565b68aaa2867954
dofalami <polina.chugaeva@gmail.com>
2022-10-30 21:42:25 +0300

    upd

--- src/randomNum.js
+++ src/randomNum.js
@@ -1,5 +1,5 @@
 const getRandom = (min, max) => Math.floor(Math.random() * (max - min)) + min;
 
-const getRandomIndex = (numbers) => getRandomNumber(0, numbers.length);
+const getRandomIndex = (numbers) => getRandom(0, numbers.length);
 
 export { getRandom, getRandomIndex };

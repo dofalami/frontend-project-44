5406117a18a3b91b2ba4f26aa315ac2a94e174f4
dofalami <polina.chugaeva@gmail.com>
2022-10-31 01:19:40 +0300

    work on mistakes :)

--- src/games/brain-calc-game.js
+++ src/games/brain-calc-game.js
@@ -2,22 +2,26 @@ import run from '../index.js';
 import { getRandom } from '../randomNum.js';
 
 const firstQuestion = 'What is the result of the expression?';
+
 const minNum = 1;
 const maxNum = 10;
 
 function randomOperator() {
   const opindex = Math.random() * 3 + 1;
   const result = Math.trunc(opindex);
-  
-  switch(result) {
+
+  switch (result) {
     case 1:
-      return '+'; 
-                  
-    case 2:      
+      return '+';
+
+    case 2:
       return '-';
-                                            
+
     case 3:
       return '*';
+
+    default:
+      return false;
   }
 }
 
@@ -27,7 +31,7 @@ function randomOperator() {
   const operator = randomOperator();
   const expression = number1 + ' ' + operator + ' ' + number2;
   const correctAnswer = eval(expression);
-  
+
   return [expression, correctAnswer];
   }
 

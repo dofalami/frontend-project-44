362029736c585a3652a5a4ba232185cb334c4989
dofalami <polina.chugaeva@gmail.com>
2022-11-01 15:05:31 +0300

    add semicolon :_)

--- src/helpers.js
+++ src/helpers.js
@@ -2,7 +2,7 @@ const getRandomNum = (min, max) => {
   const minNum = Math.ceil(min);
   const maxNum = Math.floor(max);
   return Math.floor(Math.random() * (maxNum - minNum + 1)) + minNum;
-}
+};
 
 const data = (numbers) => getRandomNum(0, numbers.length - 1);
 

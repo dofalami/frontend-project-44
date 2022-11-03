20fe7b71401c1ca9094eb2c4a898f16ed9cce66d
dofalami <polina.chugaeva@gmail.com>
2022-11-04 02:42:22 +0300

    corrections have been made, hallelujah

--- src/index.js
+++ src/index.js
@@ -17,13 +17,11 @@ export default (description, generateRound) => {
     const userAnswer = readlineSync.question('Your answer: ');
 
     if (userAnswer !== correctAnswer) {
-      console.log(`'${userAnswer}' is wrong answer ;(. Correct answer was '${correctAnswer}'. Let's try again, ${userName}!`);
-      return false;
-
+      console.log(`'${userAnswer}' is wrong answer ;(. Correct answer was '${correctAnswer}'.`);
+      console.log(`Let's try again, ${userName}!`);
+      return;
     }
     console.log('Correct!');
   }
   console.log(`Congratulations, ${userName}!`);
-
-  return [description, generateRound];
 };

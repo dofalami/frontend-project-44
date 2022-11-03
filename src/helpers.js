324f183c96cc752e8b49d51607393599dcef2d97
dofalami <polina.chugaeva@gmail.com>
2022-11-04 02:49:52 +0300

    upd!!!

--- src/helpers.js
+++ src/helpers.js
@@ -1,6 +1,4 @@
-const getRandomNumber = (min, max) => {
-  return Math.floor(Math.random() * (max - min + 1)) + min;
-};
+const getRandomNumber = (min, max) => Math.floor(Math.random() * (max - min + 1)) + min;
 
 const getRandomIndex = (data) => {
   const min = 0;

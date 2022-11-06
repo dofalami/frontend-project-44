18e89e1fe19caa88b0efdd9d2ec85f7fb9021c5a
dofalami <polina.chugaeva@gmail.com>
2022-11-07 00:53:23 +0300

    upd

--- src/games/even.js
+++ src/games/even.js
@@ -5,10 +5,11 @@ const description = 'Answer "yes" if the number is even, otherwise answer "no".'
 const minRange = 1;
 const maxRange = 100;
 
+const isEven = () => number % 2 === 0;
+
 const generateRound = () => {
   const number = getRandomNumber(minRange, maxRange);
   const question = String(number);
-  const isEven = () => question % 2 === 0;
   const correctAnswer = isEven() ? 'yes' : 'no';
 
   return [question, correctAnswer];

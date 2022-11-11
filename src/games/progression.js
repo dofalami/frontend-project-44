b07e8ebe5b04b70e940237dd860ce899d487611e
dofalami <polina.chugaeva@gmail.com>
2022-11-11 04:19:45 +0300

    upd

--- src/games/progression.js
+++ src/games/progression.js
@@ -5,27 +5,24 @@ const minStep = 2;
 const maxStep = 10;
 const minFirst = 1;
 const maxFirst = 50;
-// const lengthLimit = 10;
 
 const description = 'What number is missing in the progression?';
 
-const makeProgression = (firstNum, step, length = 10) => {
+const makeProgression = (first, step, length = 10) => {
   const progression = [];
 
   for (let i = 1; i !== length; i += 1) {
-    progression.push(firstNum + (step * i));
+    progression.push(first + (step * i));
   }
   return progression;
 };
 
 const generateRound = () => {
-  const firstNum = getRandomNumber(minFirst, maxFirst);
+  const first = getRandomNumber(minFirst, maxFirst);
   const step = getRandomNumber(minStep, maxStep);
-  const progression = makeProgression(firstNum, step);
+  const progression = makeProgression(first, step);
   const hiddenIndex = getRandomIndex(progression);
-  const number = progression[hiddenIndex];
-  progression.splice(hiddenIndex, 1, '..');
-  const correctAnswer = String(number);
+  const correctAnswer = String(progression.splice(hiddenIndex, 1, '..'));
   const question = progression.join(' ');
 
   return [question, correctAnswer];
